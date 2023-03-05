b2385d0a3adfae81d5ee4a6be21a635e458e8cb3
Nick <nholden10@gmail.com>
2023-03-04 23:49:07 -0800

    Corrected config

--- ethereum/hardhat.config.js
+++ ethereum/hardhat.config.js
@@ -1,5 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config;
+require("dotenv").config();
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const QUIKNODE_API_KEY = process.env.QUIKNODE_API_KEY;
@@ -11,8 +11,8 @@ module.exports = {
     hardhat: {},
     sepolia: {
       chainId: 11155111,
-      url: `https://dimensional-indulgent-diagram.ethereum-sepolia.discover.quiknode.pro/bf5b425b4615a80bef40e7e63716c216d0560f9c/`,
-      accounts: PRIVATE_KEY,
+      url: `https://dimensional-indulgent-diagram.ethereum-sepolia.discover.quiknode.pro/${QUIKNODE_API_KEY}/`,
+      accounts: [PRIVATE_KEY],
     },
   },
 };
